19d4c0f10a227aec9d35da5e4fe48702a91b42a5
zxc3buttons <tokare2002@gmail.com>
2022-02-02 13:35:44 +0300

    1)added @requestParam annotation to the controller as a parameter to getParameters from get requests 2)as alternative were added HttpServletRequest class as a parameter to getParameters from get requests

--- src/main/java/ru/tokarev/springmvcapp/controllers/FirstController.java
+++ src/main/java/ru/tokarev/springmvcapp/controllers/FirstController.java
@@ -3,18 +3,28 @@ package ru.tokarev.springmvcapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    public String goodByePage(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/goodbye";
     }
 
